917457832bd374ed1b534eb5dc972de4bdb27446
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2023-12-31 20:24:31 +0530

    Add files via upload

---
+++ MPU6050.ino
@@ -0,0 +1,36 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); 
+  if (!mpu.begin()) {
+    while (1) {
+      delay(10);
+    }
+  }
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  
+  delay(100);
+}
+
+void loop() {
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  //Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(" ");
+  Serial.print(a.acceleration.y);
+  Serial.print(" ");
+  Serial.print(a.acceleration.z);
+  Serial.println("");
+  delay(500);
+}
